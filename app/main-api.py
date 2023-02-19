8a547e3e2042e680703e1c44844e9922ce230af4
yunqiang.wu <yunqiang.wu@hiand-china.com>
2023-02-20 01:14:16 +0800

    fix: docker

--- app/main-api.py
+++ app/main-api.py
@@ -219,4 +219,4 @@ async def handle_broken_pipe(request, exc):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main-api:app", host="0.0.0.0", port=3000, reload=True)
+    uvicorn.run("main-api:app", host="0.0.0.0", port=3000)
